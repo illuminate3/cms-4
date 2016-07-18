847c9e5aac4b5e9db90b1e6e9781654295ef836c
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-07-18 11:13:43 -0400

    dont need this

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -20,21 +20,6 @@ class PagesController extends Controller
      */
     public function dashboard(): \Illuminate\View\View
     {
-        $campaigns = Campaign::orderBy('updated_at', 'desc')
-            ->take(5);
-
-        $rebuttals = Rebuttal::orderBy('updated_at', 'desc')
-            ->take(5);
-
-        $promos = Promo::orderBy('updated_at', 'desc')
-            ->take(5);
-
-        $terms = Terms::all();
-
-        return view('pages.dashboard')
-            ->with('campaigns', $campaigns)
-            ->with('rebuttals', $rebuttals)
-            ->with('promos', $promos)
-            ->with('terms', $terms);
+        return view('pages.dashboard');
     }
 }
