23e9c1a1b39fbc743a6a1df76c035e431df5741b
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-07-19 15:19:24 -0400

    front end validation for sections

--- resources/assets/js/components/Section/Edit.vue
+++ resources/assets/js/components/Section/Edit.vue
@@ -1,21 +1,24 @@
 <template>
     <form @submit.prevent="save" v-show="section != []">
-        <div class="form-group row">
+        <div class="form-group row" :class="{ 'has-danger': errors.name != null }">
             <label class="col-sm-3 text-xs-right">Name</label>
             <div class="col-sm-7">
                 <input type="text" v-model="section.name" class="form-control" maxlength="75">
+                <label class="form-control-label" v-show="errors.name != null">{{ errors.name }}</label>
             </div>
         </div>
-        <div class="form-group row">
+        <div class="form-group row" :class="{ 'has-danger': errors.description != null }">
             <label class="col-sm-3 text-xs-right">Description</label>
             <div class="col-sm-7">
                 <textarea v-model="section.description" class="form-control"></textarea>
+                <label class="form-control-label" v-show="errors.description != null">{{ errors.description }}</label>
             </div>
         </div>
-        <div class="form-group row">
+        <div class="form-group row" :class="{ 'has-danger': errors.content != null }">
             <label class="col-sm-3 text-xs-right">Content</label>
             <div class="col-sm-7">
                 <textarea v-model="section.content" class="form-control"></textarea>
+                <label class="form-control-label" v-show="errors.content != null">{{ errors.content }}</label>
             </div>
         </div>
         <div class="form-group row">
@@ -36,7 +39,11 @@ export default {
     data() {
         return {
             section: [],
-            errors: []
+            errors: {
+                name: null,
+                description: null,
+                content: null
+            }
         }
     },
 
