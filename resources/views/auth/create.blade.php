c7e096781f2d61ce3c2564075298ebb08101f9e9
jordanbardsley7 <jordanbardsley7@yahoo.com>
2016-08-01 15:50:02 -0400

    updated authentication view

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
     <div class="users create padded">
-        <div class="card">
+        <div class="card primary">
             <div class="card-header">Create user</div>
             <div class="card-block">
                 <create-users-component></create-users-component>
